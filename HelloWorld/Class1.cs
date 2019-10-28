68603739b59659ed1c1e05c3619870e197cf5570
lealeoni85 <leonileandroluis@gmail.com>
2019-10-28 19:49:09 -0300

    Revit

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace HelloWorld
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+        public class Class1 :IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
+        {
+
+            WindowsFormsApplication1.DBConnection DB = new WindowsFormsApplication1.DBConnection();
+
+            string sql = "SELECT * FROM edilizia.users;";
+            MySqlDataReader dataReaderInfo = DB.GetData(sql);
+            if (dataReaderInfo.HasRows)
+            {
+                DataTable dtInfo = new DataTable();
+                dtInfo.Load(dataReaderInfo);
+                TaskDialog.Show("Revit", dtInfo.Rows[0][1].ToString());
+                //lblLocal.Text = "Oficina: " + dtInfo.Rows[0][0].ToString();
+                //lblResponsable.Text = "Dueño: " + dtInfo.Rows[0][1].ToString();
+                //lblActivos.Text = "Cant. Bienes: " + dtInfo.Rows[0][2].ToString();
+                //IdRoomSelected = int.Parse(dtInfo.Rows[0][3].ToString());
+            }
+
+            TaskDialog.Show("Revit","Hola entorno!");
+            return Autodesk.Revit.UI.Result.Succeeded;
+            }
+    }
+}
---
+++ HelloWorld/DBConnection.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+
+
+namespace WindowsFormsApplication1
+{
+    class DBConnection
+    {
+        private MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter();
+
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private MySqlConnection connection;
+
+        
+        public MySqlDataReader GetData(string selectCommand)
+        {
+
+            // Specify a connection string. Replace the given value with a 
+            // valid connection string for a Northwind SQL Server sample
+            // database accessible to your system.
+            server = "localhost";
+            database = "edilizia";
+            uid = "root";
+            password = "qwe.123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(selectCommand, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                return dataReader;
+                //close connection
+                this.CloseConnection();
+            }
+            return null;
+        }
+
+        public long InsertData(string selectCommand)
+        {
+
+            // Specify a connection string. Replace the given value with a 
+            // valid connection string for a Northwind SQL Server sample
+            // database accessible to your system.
+            server = "localhost";
+            database = "edilizia";
+            uid = "root";
+            password = "qwe.123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(selectCommand, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                return cmd.LastInsertedId;
+                //close connection
+                //this.CloseConnection();
+            }
+            return 0;
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        //MessageBox.Show("Cannot connect to server. Contact administrator");
+                        break;
+                    case 1045:
+                        //MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                    default:
+                        //MessageBox.Show(ex.Message);
+                        break;
+                }
+                return false;
+            }
+        }
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        
+    }
+    
+}
